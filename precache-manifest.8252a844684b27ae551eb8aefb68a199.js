99686b08949f14f693776a3123ea28320262d009
thabisoddx <thabisomoha@gmail.com>
2020-12-21 08:17:30 +0200

    Updates

---
+++ precache-manifest.8252a844684b27ae551eb8aefb68a199.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b913cffae3e34e596c5b1bb2446a0d76",
+    "url": "/greenpoint/index.html"
+  },
+  {
+    "revision": "651f59a14f6fd15c284b",
+    "url": "/greenpoint/static/css/main.4f6720b1.chunk.css"
+  },
+  {
+    "revision": "3cdd41bc3dc9ea1d1914",
+    "url": "/greenpoint/static/js/2.817ac695.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/greenpoint/static/js/2.817ac695.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "651f59a14f6fd15c284b",
+    "url": "/greenpoint/static/js/main.72a60472.chunk.js"
+  },
+  {
+    "revision": "821bdc4461154f017ce9",
+    "url": "/greenpoint/static/js/runtime-main.fa7bbcc5.js"
+  },
+  {
+    "revision": "b630937a5d23c9b23530a698faf516a9",
+    "url": "/greenpoint/static/media/about_overlay.b630937a.webp"
+  },
+  {
+    "revision": "f1ec7a6f773a6503f62336fec072a973",
+    "url": "/greenpoint/static/media/about_part_img.f1ec7a6f.webp"
+  },
+  {
+    "revision": "791fe9f996018445e7ca6b89f4e71e3f",
+    "url": "/greenpoint/static/media/arrow-down.791fe9f9.svg"
+  },
+  {
+    "revision": "ee48d685eab75a0257038acdb3198a80",
+    "url": "/greenpoint/static/media/arrow-right-icon.ee48d685.svg"
+  },
+  {
+    "revision": "77b34d7c6bd09132ccace069e5b5a02f",
+    "url": "/greenpoint/static/media/avatar.77b34d7c.jpg"
+  },
+  {
+    "revision": "b3895a3b48dab1aa12fbbb3147e98041",
+    "url": "/greenpoint/static/media/banner_img.b3895a3b.webp"
+  },
+  {
+    "revision": "76c022998eb03ad74929cd51e8ae066c",
+    "url": "/greenpoint/static/media/blog_1.76c02299.webp"
+  },
+  {
+    "revision": "1230812f442f4c9a180f1cf513742037",
+    "url": "/greenpoint/static/media/blog_2.1230812f.webp"
+  },
+  {
+    "revision": "09b98dbcfb8f214c16df42545256b75e",
+    "url": "/greenpoint/static/media/blog_3.09b98dbc.webp"
+  },
+  {
+    "revision": "9ef1c9e7f734dbf598146106703376cc",
+    "url": "/greenpoint/static/media/comment-icon.9ef1c9e7.svg"
+  },
+  {
+    "revision": "e0720d9952bfd06387b73fd5847ab2f0",
+    "url": "/greenpoint/static/media/experiance_img.e0720d99.webp"
+  },
+  {
+    "revision": "80130e19f1a735f6b5771981abda10f7",
+    "url": "/greenpoint/static/media/experiance_overlay.80130e19.webp"
+  },
+  {
+    "revision": "e28e711ceb8f262068cd1c299e440d13",
+    "url": "/greenpoint/static/media/heading_bg.e28e711c.webp"
+  },
+  {
+    "revision": "4d3ed582723a640422ea2d8391214c59",
+    "url": "/greenpoint/static/media/heart-icon.4d3ed582.svg"
+  },
+  {
+    "revision": "c080238eeab3474d7eb39511dd3018af",
+    "url": "/greenpoint/static/media/house-icon.c080238e.svg"
+  },
+  {
+    "revision": "27488570dec1b73af0ea9d968ea22a87",
+    "url": "/greenpoint/static/media/icon-1.27488570.svg"
+  },
+  {
+    "revision": "967cc8c8a791deb906837558a5b24e80",
+    "url": "/greenpoint/static/media/icon-2.967cc8c8.svg"
+  },
+  {
+    "revision": "cb9f3c4662cd248560c5731ad2f54e50",
+    "url": "/greenpoint/static/media/icon-3.cb9f3c46.svg"
+  },
+  {
+    "revision": "47a6b90d6046f01a4254e01868d9ed81",
+    "url": "/greenpoint/static/media/icon.47a6b90d.svg"
+  },
+  {
+    "revision": "0f5dd612ad65a1162633e6423a13965e",
+    "url": "/greenpoint/static/media/menu-close.0f5dd612.svg"
+  },
+  {
+    "revision": "c512315da23da260e5859e7806a742db",
+    "url": "/greenpoint/static/media/menu-icon.c512315d.svg"
+  },
+  {
+    "revision": "18e348a97af08abaf4da5a9b8f71e47d",
+    "url": "/greenpoint/static/media/project_1.18e348a9.webp"
+  },
+  {
+    "revision": "bdbcf04dc452baecba619aa9563cdc1b",
+    "url": "/greenpoint/static/media/project_2.bdbcf04d.webp"
+  },
+  {
+    "revision": "5cd651f5a634799b987d152d5d9aebc9",
+    "url": "/greenpoint/static/media/project_3.5cd651f5.webp"
+  }
+]);
